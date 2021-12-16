972952ae933c0be17c058036ebaad1f419c64fb0
Marcus <marcus.shimotsu@gmail.com>
2021-12-16 10:32:24 -0600

    commit tree c1d1aaa86f32f396de21d6b801d3bb4faeef9eae

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(arr)
+    arr.map {|name| badge_maker(name)}
+end
+
+def assign_rooms(arr)
+    arr.map.with_index {|name,index| "Hello, #{name}! You'll be assigned to room #{index+1}!"}
+end
+
+def printer(arr)
+    arr.each {|lol| puts badge_maker(lol)}
+    assign_rooms(arr).each {|lol| puts lol}
+end
